ede29b9a87ebfa6238c85d2dd39b576d0944aad7
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-09-24 15:47:14 +0200

    Started NumbersActivity from clicking Numbers Tab on MainActivity

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -10,4 +12,5 @@ public class ColorsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
     }
+
 }
