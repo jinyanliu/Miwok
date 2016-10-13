5acbb8a56f0859c9aa0a3b8784b6e4708c68582d
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-10-13 18:45:04 +0200

    Created a numbers' array in MainActivity java file.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -82,5 +82,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+       String [] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
     }
 }
