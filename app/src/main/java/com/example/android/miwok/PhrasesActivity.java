a2ce12e289def5231db8e384cc9a9dad68a5d897
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-10-19 14:08:01 +0200

    Added code for PhrasesActivity java file. And modified activity phrases xml file. To display Phrases vocabulary.

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+
+        //Create an array list of words.
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("Where are you going?", "minto wuksus"));
+        words.add(new Word("What is your name?", "tinne oyaase'ne"));
+        words.add(new Word("My name is...", "oyaaset..."));
+        words.add(new Word("How are you feeling?", "michekses?"));
+        words.add(new Word("I'm feeling good.", "kuchi achit"));
+        words.add(new Word("Are you coming?", "eenes'aa?"));
+        words.add(new Word("Yes, I'm coming.", "hee'eenem"));
+        words.add(new Word("I'm coming.", "eenem"));
+        words.add(new Word("Let's go.", "yoowutis"));
+        words.add(new Word("Come here.", "anni'nem"));
+
+        /*
+        Create an WordAdapter, whose date source is a list of Words.
+        The adaper knows how to create list items for each item in the list.
+         */
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        /*
+        Find the ListView object in the view hierarchy of the Activity.
+        There should be a ListView with the view ID called list, which is declared in activity_numbers xml layout file.
+         */
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        /*
+        Make the ListView use the WordAdapter we created above, so that the ListView will display list items for each word in the list.
+         */
+        listView.setAdapter(adapter);
     }
 }
