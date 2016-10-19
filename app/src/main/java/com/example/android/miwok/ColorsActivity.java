06a8948d4bc995be2c3cfdf824161708dba0f678
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-10-19 14:02:15 +0200

    Added code for ColorsActivity java file. And modified activity colors xml file. To display Colors vocabulary.

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,9 +1,10 @@
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -11,6 +12,35 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        //Create an array list of words.
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("red", "wetetti"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "takaakki"));
+        words.add(new Word("gray", "topoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "topiise"));
+        words.add(new Word("mustard yellow", "chiwiite"));
+
+        /*
+        Create an WordAdapter, whose date source is a list of Words.
+        The adaper knows how to create list items for each item in the list.
+         */
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        /*
+        Find the ListView object in the view hierarchy of the Activity.
+        There should be a ListView with the view ID called list, which is declared in activity_numbers xml layout file.
+         */
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        /*
+        Make the ListView use the WordAdapter we created above, so that the ListView will display list items for each word in the list.
+         */
+        listView.setAdapter(adapter);
     }
 
 }
