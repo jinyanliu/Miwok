0f5cf42e5ee0c4cd66d8e02373560c8201f12687
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-10-19 13:57:38 +0200

    Added code for FamilyActivity java file. And modified activity members xml file. To display Family Members vocabulary.

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_members);
+
+        //Create an array list of words.
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("father", "epe"));
+        words.add(new Word("mother", "eta"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "tete"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
+        /*
+        Create an WordAdapter, whose date source is a list of Words.
+        The adaper knows how to create list items for each item in the list.
+         */
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        /*
+        Find the ListView object in the view hierarchy of the Activity.
+        There should be a ListView with the view ID called list, which is declared in activity_numbers xml layout file.
+         */
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        /*
+        Make the ListView use the WordAdapter we created above, so that the ListView will display list items for each word in the list.
+         */
+        listView.setAdapter(adapter);
     }
 }
