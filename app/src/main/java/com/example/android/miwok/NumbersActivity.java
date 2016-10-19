ac25d32522a2a6ff7506acbdbd207effa4b569c1
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-10-19 13:43:39 +0200

    Deleted all the unused comments and codes.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -13,38 +13,8 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-//        //Create an array of words.
-//        String [] words = new String[10];
-//        words[0] = "one";
-//        words[1] = "two";
-//        words[2] = "three";
-//        words[3] = "four";
-//        words[4] = "five";
-//        words[5] = "six";
-//        words[6] = "seven";
-//        words[7] = "eight";
-//        words[8] = "nine";
-//        words[9] = "ten";
-//
-//        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
-//        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
-//        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
-//        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
-//        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
-//        Log.v("NumbersActivity", "Word at index 5: " + words[5]);
-//        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
-//        Log.v("NumbersActivity", "Word at index 7: " + words[7]);
-//        Log.v("NumbersActivity", "Word at index 8: " + words[8]);
-//        Log.v("NumbersActivity", "Word at index 9: " + words[9]);
-
-
         //Create an array list of words.
         ArrayList<Word> words = new ArrayList<Word>();
-
-        //words.add("one");
-
-//        Word w = new Word("one", "lutti");
-//        words.add(w);
 
         words.add(new Word("one", "lutti"));
         words.add(new Word("two", "otiiko"));
@@ -57,69 +27,6 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("night", "wo'e"));
         words.add(new Word("ten", "na'aacha"));
 
-
-//        // Verify the contents of the ArrayList by printing out each ArrayList element to the logs
-//        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
-//        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
-//        Log.v("NumbersActivity", "Word at index 2: " + words.get(2));
-//        Log.v("NumbersActivity", "Word at index 3: " + words.get(3));
-//        Log.v("NumbersActivity", "Word at index 4: " + words.get(4));
-//        Log.v("NumbersActivity", "Word at index 5: " + words.get(5));
-//        Log.v("NumbersActivity", "Word at index 6: " + words.get(6));
-//        Log.v("NumbersActivity", "Word at index 7: " + words.get(7));
-//        Log.v("NumbersActivity", "Word at index 8: " + words.get(8));
-//        Log.v("NumbersActivity", "Word at index 9: " + words.get(9));
-
-//        // Manually add textviews.
-//        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-//        TextView wordView = new TextView(this);
-//        wordView.setText(words.get(0));
-//        rootView.addView(wordView);
-
-//        //Find the root view of the whole layout
-//        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-
-//        /*
-//        Setup counter variable
-//        Create a variable to keep track of the current index position
-//         */
-//        int index = 0;
-//
-//        while (index < words.size()) {
-//
-//           /*
-//           Instructions
-//           Create a new TextView that displays the word
-//           add the view as a child to the rootView
-//            */
-//            TextView wordView = new TextView(this);
-//            wordView.setText(words.get(index));
-//            rootView.addView(wordView);
-//
-//            //Update counter variable
-//            index++; //index = index + 1
-//
-//        }
-
-//        for (int index = 0; index < words.size(); index++) {
-//
-//            //Create a new TextView
-//            TextView wordView = new TextView(this);
-//
-//            //Set the text to be word at the current index
-//            wordView.setText(words.get(index));
-//
-//            //Add this TextView as another child to the root view of this layout
-//            rootView.addView(wordView);
-//        }
-
-        /*
-        Create an ArrayAdapter, whose data source is a list of String.
-        The adapter knows how to create layouts for each item in the list, using the simple_list_item_1.xml layout resource defined in the Android framwork.
-        This list item layout contains a single TextView, which the adapter will set to display a single word.
-         */
-        //ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<Word>(this, R.layout.list_item, words);
-
         /*
         Create an WordAdapter, whose date source is a list of Words.
         The adaper knows how to create list items for each item in the list.
@@ -131,12 +38,6 @@ public class NumbersActivity extends AppCompatActivity {
         There should be a ListView with the view ID called list, which is declared in activity_numbers xml layout file.
          */
         ListView listView = (ListView) findViewById(R.id.list);
-
-        /*
-        Make the ListView use the ArrayAdapter we created above, so that the List View will display list items for each word in the list of words.
-        Do this by calling the setAdapter method on the ListView object and pass in 1 argument, which is the ArrayAdapter with the variable name itemsAdapter.
-         */
-        //listView.setAdapter(itemsAdapter);
 
         /*
         Make the ListView use the WordAdapter we created above, so that the ListView will display list items for each word in the list.
