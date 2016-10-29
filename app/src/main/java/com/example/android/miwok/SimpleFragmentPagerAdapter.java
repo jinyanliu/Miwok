33056d33568669e21ab8728432a648ae69019d3f
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-10-29 22:25:16 +0200

    Add detailed comments.

--- app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
+++ app/src/main/java/com/example/android/miwok/SimpleFragmentPagerAdapter.java
@@ -9,14 +9,23 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 /**
- * Provides the appropriate {@link Fragment} for a view pager.
+ * SimpleFragmentPagerAdaper is a FragmentPagerAdapter that can provide the layout for each list item based on a data source which is a list of Word objects.
  */
 public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
 
+    /**
+     * Create a new SimpleFragmentPagerAdapter object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter across swipes.
+     */
     public SimpleFragmentPagerAdapter(FragmentManager fm) {
         super(fm);
     }
 
+    /**
+     * @param position
+     * @return the Fragment that should be displayed for the given page number.
+     */
     @Override
     public Fragment getItem(int position) {
         if (position == 0) {
@@ -29,6 +38,9 @@ public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
             return new PhrasesFragment();
     }
 
+    /**
+     * @return the total number of pages.
+     */
     @Override
     public int getCount() {
         return 4;
